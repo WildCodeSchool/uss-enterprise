9dcc32987d14bd5eb67ebe990e11304cbd199bd6
alejolale <sergio.prieto01@outlook.com>
2019-10-17 22:41:17 +0200

    Add image,button, css style

--- src/Components/home-page/homePage.js
+++ src/Components/home-page/homePage.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './homePage.css'
-const HomePage= () =>{
-    return (
-    <div>
-        <h1>Game name</h1>
-        <button>Start</button>
-        <div>
 
-        <img className="planet-picture" src="../img/Saturnx.png" alt='planet'/>
+const HomePage= () =>{
+    //--obtenir une liste de planetes pour afficher random ;)
+    const planet = require('../img/Saturnx.png');
+    return (
+    <div className="space-container">
+
+        <h1 className="game-title">Game name</h1>
+
+        <div class="round-button">
+            <div class="round-button-circle">
+                <a href="#" class="round-button">START</a>
+            </div>
         </div>
-        <footer>
+
+       
+        <img className="planet-picture" src={planet} alt='planet'/>
+       
+        <footer className="footer">
             <p>Projet pour la Wild Code School</p>
         </footer>
+
     </div>);
 }
 
