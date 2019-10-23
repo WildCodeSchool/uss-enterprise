bc1df2033ac3a2ecb26e5bac5aaba465bd1f4263
François Boisselier <francois.boisselier@gmail.com>
2019-10-23 17:12:54 +0200

    ipad ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
+import Intropage from './Components/Intropage/Intropage.js'
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <Intropage/>
     </div>
   );
 }
