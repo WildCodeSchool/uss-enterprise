ce838d8cc294bbc25ccbd9f6f64227af95454fe9
alejolale <sergio.prieto01@outlook.com>
2019-10-23 13:43:35 +0200

    Add navBar class jsx & css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
+import NavBar from './components/navBar/navBar';
 import Quizz from './components/Quizz/Quizz';
 import { Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Route exact path="/">
+        <NavBar />
         <HomePage />
       </Route>
       <Route path="/quizz">
