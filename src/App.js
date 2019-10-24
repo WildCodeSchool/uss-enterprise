a0fd7899cdd79ae2856bb9c13b56c89183560877
alejolale <sergio.prieto01@outlook.com>
2019-10-24 17:46:28 +0200

    Add PascalCase on folders and documents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/homePage/homePage';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
