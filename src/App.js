d94e380923dc4e3a0585d4f091ae6565317dd00f
Eduardo <eduardo.marselle@gmail.com>
2019-10-28 17:27:34 +0100

    ajout de la police star wars

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import HomePage from './components/HomePage/HomePage';
 import Quizz from './Components/Quizz/Quizz';
 
+
 function App() {
   return (
     <div className="App">
--- src/Components/Quizz/StarsQuizz.js
+++ src/Components/Quizz/StarsQuizz.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './StarsQuizz.css';
 
-const StarsQuizz = ({difficulty}) => {
+class StarsQuizz extends Component {
 
-    const difHard = <ul><li className='etoilePleine'></li><li className='etoilePleine'></li><li className='etoilePleine'></li>
-    <li className='etoilePleine'></li><li className='etoilePleine'></li></ul>;
-    const difMedium = <ul><li className='etoilePleine'></li><li className='etoilePleine'></li><li className='etoileMi'></li>
-    <li className='etoileVide'></li><li className='etoileVide'></li></ul>
-    const difEasy = <ul><li className='etoileVide'></li><li className='etoileVide'></li><li className='etoileVide'></li>
-    <li className='etoileVide'></li><li className='etoileVide'></li></ul>
+    state = {
+        difficulty : 'easy'
+    }
 
+    render() {
+        const difHard = <ul><li className='etoilePleine'></li><li className='etoilePleine'></li><li className='etoilePleine'></li>
+        <li className='etoilePleine'></li><li className='etoilePleine'></li></ul>;
+        const difMedium = <ul><li className='etoilePleine'></li><li className='etoilePleine'></li><li className='etoileMi'></li>
+        <li className='etoileVide'></li><li className='etoileVide'></li></ul>
+        const difEasy = <ul><li className='etoileVide'></li><li className='etoileVide'></li><li className='etoileVide'></li>
+        <li className='etoileVide'></li><li className='etoileVide'></li></ul>
+
+console.log(this.state.difficulty)
+
+        return (
+            <>
+            <select onChange={ (even) => this.setState({difficulty : even.target.value}) } className='selectDifficulty ' name="difficulty" id="difficulty-select">
+                    <option value="">choose a difficulty</option>
+                    <option value="easy">easy</option>
+                    <option value="medium">medium</option>
+                    <option value="hard">hard</option>
+            </select> 
     
-
-    return (
-        <div className='divEtoile'>
-        { difficulty === 'easy' ? difEasy : <p></p> }
-        { difficulty === 'medium' ? difMedium : <p></p> }
-        { difficulty === 'hard' ? difHard : <p></p> }
-        </div>
-     )} 
+            <div className='divEtoile'>
+            { this.state.difficulty === 'easy' ? difEasy : <p></p> }
+            { this.state.difficulty === 'medium' ? difMedium : <p></p> }
+            { this.state.difficulty === 'hard' ? difHard : <p></p> }
+            </div>
+            </>
+         )} 
+    }
 
 
 export default StarsQuizz
