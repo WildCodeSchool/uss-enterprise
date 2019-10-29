e0ebaf34b368ca689e90929dcbf07a84e9136112
Eduardo <eduardo.marselle@gmail.com>
2019-10-29 15:32:24 +0100

    28 probleme sur 32 résolut de la pull request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import './App.css';
-import Quizz from './Components/Quizz/Quizz'
-
+import React from "react";
+import "./App.css";
+import Quizz from "./Components/Quizz/Quizz";
 
 function App() {
   return (
