4e6c8d3a7456c5947ebedfb1f5889e64e36e8537
Eduardo <eduardo.marselle@gmail.com>
2019-11-03 23:34:03 +0100

    résolution de la seconde pull request

--- src/Components/Quizz/Proposition/PropositionsQuizz.js
+++ src/Components/Quizz/Proposition/PropositionsQuizz.js
@@ -1,111 +1,88 @@
-import React, { Component } from "react";
-import "./PropositionsQuizz.css";
-import Modal from "./Modal";
+import React , {Component} from 'react';
+import './PropositionsQuizz.css';
+import Modal from '/home/eduardo/Quetes Wild Code School/projet-uss/uss-enterprise/src/Components/Quizz/Modal/Modal.js'
+
 
 class PropositionsQuizz extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      isShowing: false,
-      resultatQuestion: false,
-    };
-  }
-
-  openModalHandler = () => {
-    this.setState({
-      isShowing: true
-    });
-  };
-
-  closeModalHandler = () => {
-    this.setState({
-      isShowing: false
-    });
-  };
-
-
-  hide(e) {
-    if (e.currentTarget.outerText === this.props.bonneReponse) {
-      this.setState({ resultatQuestion: true });
-    } else {
-      this.setState({ resultatQuestion: false });
+        this.state = {
+            isShowing: false,
+            questionResult: false,
+        }
     }
-  }
 
-  getReponse() {
-    if (!this.props.mauvaiseReponse) return null;
-    let totalReponse = [this.props.bonneReponse];
-    totalReponse.push(
-      this.props.mauvaiseReponse[0],
-      this.props.mauvaiseReponse[1],
-      this.props.mauvaiseReponse[2]
-    );
-    totalReponse.sort(function(a, b) {
-      if (a < b) {
-        return 1;
-      }
-      if (a > b) {
-        return -1;
-      }
-      return 0;
-    });
-
-    let propositionsReponses = [];
-
-    if (this.props.mauvaiseReponse) {
-      propositionsReponses = totalReponse.map((reponses, index) => (
-        <div key={index}>
-          <input className='champReponse' type="radio" name="name" id={`propo ${index}`} />
-          <label
-            onClick={this.hide.bind(this)}
-            htmlFor={`propo ${index}`}
-            className="proposition"
-          >
-            {reponses}
-          </label>
-        </div>
-      ));
+    openModalHandler = () => {
+        this.setState({
+            isShowing: true
+        });
     }
-    return propositionsReponses;
-  }
 
-  render() {
-    return (
-      <div>
-        {this.state.isShowing ? (
-          <div onClick={this.closeModalHandler} className="back-drop"></div>
-        ) : null}
+    closeModalHandler = () => {
+        this.setState({
+            isShowing: false
+        });
+    }
 
-        {this.getReponse()}
-        <div>
-          <button
-            className="open-modal-btn buttonQuizz"
-            onClick={this.openModalHandler}
-          >
-            Validez Votre Réponse
-          </button>
+    hide = (e) => {
+        const { correctAnswer } = this.props;
+        this.setState({
+          questionResult: e.currentTarget.outerText === correctAnswer
+        })
+    }
+       
 
-          <Modal
-            className="modal"
-            show={this.state.isShowing}
-            close={this.closeModalHandler}
-            resultat={this.state.resultatQuestion}
-          >
-            {this.state.resultatQuestion ? (
-              <p>
-                Congratulation !!! <br></br> Your answer is true !!!
-              </p>
-            ) : (
-              <p>
-                bad luck !!! <br></br> Your answer is false
-              </p>
-            )}
-          </Modal>
-        </div>
-      </div>
-    );
-  }
+   renderReponse() {
+    const { incorrectAnswers, correctAnswer } = this.props;
+
+    if (!incorrectAnswers) return null;
+
+    const randomlySortedAnswers = [correctAnswer, ...incorrectAnswers]
+        .sort((a, b) => {
+            if (a < b) {
+                return 1 
+            } else
+            if (a > b) {
+                return -1
+            }
+            return 0;
+        })
+        .map((answer, i) => (
+            <div key={answer}>
+                <input className="champReponse" type="radio" name="name" id={`propo-${i}`}/>
+                <label onClick={this.hide} htmlFor={`propo-${i}`} className='proposition'>{answer}</label>
+            </div>
+        ));
+    
+    return randomlySortedAnswers;
 }
 
-export default PropositionsQuizz;
+
+    render() {
+        const { isShowing, questionResult } = this.state;
+        return (
+            <div>
+                {isShowing && <div onClick={this.closeModalHandler} className="back-drop"></div>}
+                {this.renderReponse()}
+                <div>
+                    <button 
+                        className="open-modal-btn buttonQuizz"
+                        onClick={this.openModalHandler}
+                    >
+                        Validez Votre Réponse
+                    </button>
+                    <Modal
+                        className="modal"
+                        show={isShowing}
+                        close={this.closeModalHandler}
+                        resultat={questionResult}
+                    >
+                    </Modal>
+                </div>
+            </div>
+        )}
+}
+
+
+export default PropositionsQuizz
