b52446d0b9225a7986b9179606281e6acd1e6a76
alejolale <sergio.prieto01@outlook.com>
2019-11-04 17:17:27 +0100

    Add style mobile

---
+++ src/components/FactsNasa/FactDetail/FactDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./FactDetail.css";
+
+const FactDetail = fact => {
+  return (
+    <div className="FactDetail">
+      <h3 className="FactDetail__title">{fact.title} </h3>
+      <img className="FactDetail__picture" src={fact.url} alt={fact.title} />
+      <div className="FactDetail__description">
+        <h4 className="FactDetail__suggest">
+          <img
+            src="https://img.icons8.com/office/50/000000/vulcan-head.png"
+            alt="hand"
+          />
+          Did you know?
+        </h4>
+        <p className="FactDetail__explanation">{fact.explanation}</p>
+      </div>
+    </div>
+  );
+};
+
+export default FactDetail;
