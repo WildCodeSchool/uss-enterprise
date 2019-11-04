c4a6e49afe70ce76b5edb73bda9a5b76f4656099
François Boisselier <francois.boisselier@gmail.com>
2019-11-04 14:46:41 +0100

    pull request changes

---
+++ src/components/HyperSpace/HyperSpace.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Hyperspace() {
+  return (
+    <div className="video">
+      
+      <Player>
+        <source src="src/videos/space.mp4" />
+      </Player>
+       
+        </div>
+  )
+}
+
+export default Hyperspace;
---
+++ src/components/IntroPage/IntroPage.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./IntroPage.css";
+import image from "./Alien.png";
+
+function IntroPage() {
+  return (
+    <div className="alien_container">
+      <div className="bubble_text_container">
+        <div className="bubble_text">
+          <p className="hello_text">
+            <div className="yoda-guide">
+              Hello ! I'm YODA, your guide. Travel from planets to planets,
+              answer the questions to complete your journey
+            </div>
+            READY ARE YOU? GO !
+          </p>
+          <p className="tell_name">
+            {" "}
+            But if beguin you want, first your name you need to tell me
+          </p>
+        </div>
+      </div>
+      <div>
+        <img src={image} className="alien"></img>
+      </div>
+      <div className="form_container">
+        <form method="post" className="player-form">
+          <div className="p_container">
+            <label className="label"> </label>{" "}
+            <input
+              class="player-form-input"
+              type="text"
+              placeholder="Your Name"
+              name="pseudo"
+            />
+          </div>
+          <div className="button_container">
+            <button className="button" type="submit">
+              GO
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default IntroPage;
