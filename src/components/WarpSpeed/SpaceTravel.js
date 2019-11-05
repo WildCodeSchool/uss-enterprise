95a38a033936fbc6f0d76ea42510eda0480498d4
alejolale <sergio.prieto01@outlook.com>
2019-11-05 11:24:03 +0100

    Add basic SpaceTravel component

---
+++ src/components/WarpSpeed/SpaceTravel.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import WarpSpeed from './WarpSpeed'
+import './Spacetravel.css';
+
+class SpaceTravel extends React.Component {
+    
+    handleEvent = () => {
+
+        //here we define different speeds for showing the effect
+        //const lowSpeed = '"speed":0.7,"speedAdjFactor":0.03,"density":0.7,"shape":"circle","warpEffect":true,"warpEffectLength":5,"depthFade":true,"starSize":3,"backgroundColor":"hsl(263,45%,7%)","starColor":"#FFFFFF"';
+
+        
+        
+        const x = new WarpSpeed('SpaceTravel__fond');
+        x.SPEED = 100;
+        x.BACKGROUND_COLOR = "hsl(0,0%,7%)";
+        
+        console.log(x);
+    }
+
+    componentDidMount() {
+        this.handleEvent();
+    }
+    
+    
+    render() {
+        
+        return (
+            <div className="Spacetravel">
+              
+                <canvas id="SpaceTravel__fond" className="SpaceTravel__fond"/>
+              
+            </div>
+        );
+    }
+
+
+}
+
+export default SpaceTravel;
