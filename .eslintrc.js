d82e80d40243e619c0bd6287b8261a7fae899203
Nombalier <a.nombalier@gmail.com>
2019-11-06 10:20:59 +0100

    modification eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,7 @@
 module.exports = {
      extends: "airbnb",
      rules : {
-<<<<<<< HEAD
           'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
           "react/prop-types": [0]
      }
 };
-=======
-          'react/jsx-filename-extension': [1, { extension: ['.js'] }]
-     }
-     };
->>>>>>> add ESLint
