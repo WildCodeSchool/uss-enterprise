4000181c51bb20236f5b6ec2dc4345f259cded16
Eduardo <eduardo.marselle@gmail.com>
2019-11-06 10:30:48 +0100

    sauvegarde + indentation avant PR

--- src/Components/Quizz/Proposition/PropositionsQuizz.js
+++ src/Components/Quizz/Proposition/PropositionsQuizz.js
@@ -1,88 +1,96 @@
-import React , {Component} from 'react';
-import './PropositionsQuizz.css';
-import Modal from '../Modal/Modal'
-
+import React, { Component } from "react";
+import "./PropositionsQuizz.css";
+import Modal from "../Modal/Modal";
 
 class PropositionsQuizz extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            isShowing: false,
-            questionResult: false,
-        }
-    }
+    this.state = {
+      isShowing: false,
+      questionResult: false
+    };
+  }
 
-    openModalHandler = () => {
-        this.setState({
-            isShowing: true
-        });
-    }
+  openModalHandler = () => {
+    this.setState({
+      isShowing: true
+    });
+  };
 
-    closeModalHandler = () => {
-        this.setState({
-            isShowing: false
-        });
-    }
+  closeModalHandler = () => {
+    this.setState({
+      isShowing: false
+    });
+  };
 
-    hide = (e) => {
-        const { correctAnswer } = this.props;
-        this.setState({
-          questionResult: e.currentTarget.outerText === correctAnswer
-        })
-    }
-       
+  hide = e => {
+    const { correctAnswer } = this.props;
+    this.setState({
+      questionResult: e.currentTarget.outerText === correctAnswer
+    });
+  };
 
-   renderReponse() {
+  renderReponse() {
     const { incorrectAnswers, correctAnswer } = this.props;
 
     if (!incorrectAnswers) return null;
 
     const randomlySortedAnswers = [correctAnswer, ...incorrectAnswers]
-        .sort((a, b) => {
-            if (a < b) {
-                return 1 
-            } else
-            if (a > b) {
-                return -1
-            }
-            return 0;
-        })
-        .map((answer, i) => (
-            <div key={answer}>
-                <input className="champReponse" type="radio" name="name" id={`propo-${i}`}/>
-                <label onClick={this.hide} htmlFor={`propo-${i}`} className='proposition'>{answer}</label>
-            </div>
-        ));
-    
+      .sort((a, b) => {
+        if (a < b) {
+          return 1;
+        } else if (a > b) {
+          return -1;
+        }
+        return 0;
+      })
+      .map((answer, i) => (
+        <div key={answer}>
+          <input
+            className="champReponse"
+            type="radio"
+            name="name"
+            id={`propo-${i}`}
+          />
+          <label
+            onClick={this.hide}
+            htmlFor={`propo-${i}`}
+            className="proposition"
+          >
+            {answer}
+          </label>
+        </div>
+      ));
+
     return randomlySortedAnswers;
-}
+  }
 
-
-    render() {
-        const { isShowing, questionResult } = this.state;
-        return (
-            <div>
-                {isShowing && <div onClick={this.closeModalHandler} className="back-drop"></div>}
-                {this.renderReponse()}
-                <div>
-                    <button 
-                        className="open-modal-btn buttonQuizz"
-                        onClick={this.openModalHandler}
-                    >
-                        validez votre réponse
-                    </button>
-                    <Modal
-                        className="modal"
-                        show={isShowing}
-                        close={this.closeModalHandler}
-                        resultat={questionResult}
-                    >
-                    </Modal>
-                </div>
-            </div>
+  render() {
+    const { isShowing, questionResult } = this.state;
+    return (
+      <div>
+        {isShowing && (
+          <div onClick={this.closeModalHandler} className="back-drop"></div>
         )}
+        {this.renderReponse()}
+        <div>
+          <button
+            className="open-modal-btn buttonQuizz"
+            onClick={this.openModalHandler}
+          >
+            validez votre réponse
+          </button>
+          <Modal
+            className="modal"
+            show={isShowing}
+            close={this.closeModalHandler}
+            resultat={questionResult}
+          ></Modal>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-export default PropositionsQuizz
+export default PropositionsQuizz;
--- src/Components/Quizz/QuizzStars/StarsQuizz.js
+++ src/Components/Quizz/QuizzStars/StarsQuizz.js
@@ -1,35 +1,38 @@
-import React from 'react';
-import './StarsQuizz.css';
+import React from "react";
+import "./StarsQuizz.css";
 
 const StarsQuizz = ({ difficulty, changeDiffculty }) => {
-    return (
-        <>
-            <select onChange={ changeDiffculty } className='selectDifficulty ' name="difficulty" id="difficulty-select">
-                    <option value="">choose a difficulty</option>
-                    <option value="easy">easy</option>
-                    <option value="medium">medium</option>
-                    <option value="hard">hard</option>
-            </select> 
-      
+  return (
+    <>
+      <select
+        onChange={changeDiffculty}
+        className="selectDifficulty "
+        name="difficulty"
+        id="difficulty-select"
+      >
+        <option value="">choose a difficulty</option>
+        <option value="easy">easy</option>
+        <option value="medium">medium</option>
+        <option value="hard">hard</option>
+      </select>
 
-            <ul className="divStars ">
-                <li className='starsFull'></li>
-                <li className={
-                    difficulty === 'easy'
-                        ? 'starsEmpty' : 'starsFull' }></li>
-                <li className={
-                    difficulty === 'easy'
-                        ? 'starsEmpty'
-                        : difficulty === 'medium'
-                            ? 'starsHalf'
-                            : 'starsFull'
-                }></li>
-                <li className={difficulty === 'hard' ? 'starsFull' : 'starsEmpty'}></li>
-                <li className={difficulty === 'hard' ? 'starsFull' : 'starsEmpty'}></li>
-            </ul>
-        </>
-     )
-} 
+      <ul className="divStars ">
+        <li className="starsFull"></li>
+        <li className={difficulty === "easy" ? "starsEmpty" : "starsFull"}></li>
+        <li
+          className={
+            difficulty === "easy"
+              ? "starsEmpty"
+              : difficulty === "medium"
+              ? "starsHalf"
+              : "starsFull"
+          }
+        ></li>
+        <li className={difficulty === "hard" ? "starsFull" : "starsEmpty"}></li>
+        <li className={difficulty === "hard" ? "starsFull" : "starsEmpty"}></li>
+      </ul>
+    </>
+  );
+};
 
-
-export default StarsQuizz
+export default StarsQuizz;
