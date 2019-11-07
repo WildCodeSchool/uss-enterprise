bc648e67a1aff76566eb192f65121a4abca17c58
Adrien Nombalier <adri1n.apple@gmail.com>
2019-11-07 10:48:48 +0100

    add router and link between HomePage and Quizz

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
 import Quizz from './components/Quizz/Quizz';
+import { Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <Quizz />
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route path="/quizz">
+        <Quizz />
+      </Route>
     </div>
   );
 }
