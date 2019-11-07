f218eac044bcf4a5e0660a00aa00c5ba009a53b2
Adrien Nombalier <adri1n.apple@gmail.com>
2019-11-07 10:26:11 +0100

    fix conflit and indentations

--- src/components/Answers/Answer.js
+++ src/components/Answers/Answer.js
@@ -1,26 +1,21 @@
-import React from "react";
+import React from 'react';
 
-const Answer = ({ answer, id, checkCorrectAnswer }) => {
-    return (
-        <div key={answer}>
-          <input
-            className="champReponse"
-            type="radio"
-            name="name"
-            id={id}
-          />
-          <label
-            htmlFor={id}
-            className="proposition"
-            onClick={checkCorrectAnswer}
-          >
-            {answer}
-          </label>
-        </div>
-    )
+const Answer = ({ answer, id, checkCorrectAnswer }) => (
+  <div key={answer}>
+    <input
+      className="champReponse"
+      type="radio"
+      name="name"
+      id={id}
+    />
+    <label
+      htmlFor={id}
+      className="proposition"
+      onClick={checkCorrectAnswer}
+    >
+      {answer}
+    </label>
+  </div>
+);
 
-}
-
-
-
-export default Answer
+export default Answer;
