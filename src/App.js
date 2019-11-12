eca159e92f91c788e3d9ec10f1bda7d9f01ba0c9
François Boisselier <francois.boisselier@gmail.com>
2019-11-12 10:19:52 +0100

    change policeand form effective

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import HomePage from './components/HomePage/HomePage';
-import IntroPage from './components/IntroPage/IntroPage'
+
+import IntroPage from './components/IntroPage/IntroPage';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
       <IntroPage/>
     </div>
   );
