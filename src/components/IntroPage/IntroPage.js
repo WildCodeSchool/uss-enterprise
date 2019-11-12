3e3fbf453aeef1e54fd3d7d9590c767beb6bb4c6
François Boisselier <francois.boisselier@gmail.com>
2019-11-12 10:19:52 +0100

    change policeand form effective

--- src/components/IntroPage/IntroPage.js
+++ src/components/IntroPage/IntroPage.js
@@ -1,48 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import "./IntroPage.css";
 import image from "./Alien.png";
 
-function IntroPage() {
-  return (
-    <div className="alien_container">
-      <div className="bubble_text_container">
-        <div className="bubble_text">
-          <p className="hello_text">
-            <div className="yoda-guide">
-              Hello ! I'm YODA, your guide. Travel from planets to planets,
-              answer the questions to complete your journey
+
+class IntroPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value : ''}; 
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  
+  render() {
+    return (
+      <div className="IntroPage">
+        <div className="top_container">
+          <div className="bubble_text_container">
+            <div className="bubble_text">
+              <p className="hello_text">
+                <div className="yoda-guide">
+                  Hello ! I'm YODA, your guide. Travel from planets to planets,
+                  answer the questions to complete your journey
+                </div>
+                READY ARE YOU? GO !
+              </p>
+              <p className="tell_name">
+                {" "}
+                But if beguin you want, first your name you need to tell me
+              </p>
             </div>
-            READY ARE YOU? GO !
-          </p>
-          <p className="tell_name">
-            {" "}
-            But if beguin you want, first your name you need to tell me
-          </p>
+          </div>
+          <div className="alien_container">
+            <img src={image} className="alien" alt="alien"></img>
+          </div>
+        </div>
+        <div className="form_container">
+          <form method="post" className="player-form">
+            <div className="p_container">
+              <label className="label"> </label>{" "}
+              <input
+                value = {this.state.value}
+                onChange = {this.handleChange}
+                class="player-form-input"
+                type="text"
+                placeholder="Your Name"
+                name="pseudo"
+              />
+            </div>
+            <div className="button_container">
+              <button className="button" type="submit">
+                GO
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-      <div>
-        <img src={image} className="alien"></img>
-      </div>
-      <div className="form_container">
-        <form method="post" className="player-form">
-          <div className="p_container">
-            <label className="label"> </label>{" "}
-            <input
-              class="player-form-input"
-              type="text"
-              placeholder="Your Name"
-              name="pseudo"
-            />
-          </div>
-          <div className="button_container">
-            <button className="button" type="submit">
-              GO
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default IntroPage;
