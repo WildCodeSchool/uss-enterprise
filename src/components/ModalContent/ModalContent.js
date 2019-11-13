5462f7b89e0f8f6dde75d0d2cf43c751d0be4a41
Eduardo <eduardo.marselle@gmail.com>
2019-11-13 10:13:15 +0100

    fix problems

--- src/components/ModalContent/ModalContent.js
+++ src/components/ModalContent/ModalContent.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { Button, Modal } from "react";
+import { Button, Modal } from "react-bootstrap";
 import "./ModalContent.css";
+<<<<<<< HEAD:src/components/map_progression/ModalContent/ModalContent.js
+=======
+import { Link } from 'react-router-dom';
+>>>>>>> fix problems:src/components/ModalContent/ModalContent.js
 
 class ModalContent extends React.Component {
   constructor(props) {
