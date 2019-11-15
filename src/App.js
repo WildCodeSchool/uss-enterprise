3addaedc602a76846ec06e7ec86b130cde0353f3
sergio Prieto <saprieto5@gmail.com>
2019-11-15 12:08:50 +0100

    add css into maps modal

--- src/App.js
+++ src/App.js
@@ -84,7 +84,7 @@ class App extends Component {
               <div className="twinkling">
                 <div className="clouds">
                   <NavBar points={points} name={name} />
-                  <SolarSystem />
+                  <SolarSystem name={name}/>
                 </div>
               </div>
             </div>
