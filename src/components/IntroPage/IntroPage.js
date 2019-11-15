875b3443caff86c60e1c21e2afcdfacff1d79e84
Eduardo <eduardo.marselle@gmail.com>
2019-11-15 13:36:31 +0100

    pas bonne sauvegarde

--- src/components/IntroPage/IntroPage.js
+++ src/components/IntroPage/IntroPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Redirect } from "react";
 import "./IntroPage.css";
 import { Link } from "react-router-dom";
 import image from "./Alien.png";
